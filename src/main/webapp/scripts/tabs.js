69813628ba791984bfed2684db99a722f35bdebd
aleksandr-st <aleksandr.stupakov.2014@gmail.com>
2015-04-14 01:21:33 +0300

    More changes for web view

--- src/main/webapp/scripts/tabs.js
+++ src/main/webapp/scripts/tabs.js
@@ -5,11 +5,13 @@ $(document).ready(function(){
         $("#contactHobbiesTab").addClass("hiddenTab");
         $("#contactPlacesTab").addClass("hiddenTab");
         $("#contactFriendsTab").addClass("hiddenTab");
+        $("#contactDetailsTab").addClass("hiddenTab");
         
         $("#contactData").addClass("activeTab");
         $("#contactHobbies").removeClass("activeTab");
         $("#contactPlaces").removeClass("activeTab");
         $("#contactFriends").removeClass("activeTab");
+        $("#contactDetails").removeClass("activeTab");
     });
 
     $("#contactHobbies").click(function(){
@@ -17,11 +19,13 @@ $(document).ready(function(){
         $("#contactHobbiesTab").removeClass("hiddenTab");
         $("#contactPlacesTab").addClass("hiddenTab");
         $("#contactFriendsTab").addClass("hiddenTab");
+        $("#contactDetailsTab").addClass("hiddenTab");
         
         $("#contactData").removeClass("activeTab");
         $("#contactHobbies").addClass("activeTab");
         $("#contactPlaces").removeClass("activeTab");
         $("#contactFriends").removeClass("activeTab");
+        $("#contactDetails").removeClass("activeTab");
     });
 
     $("#contactPlaces").click(function(){
@@ -29,11 +33,13 @@ $(document).ready(function(){
         $("#contactHobbiesTab").addClass("hiddenTab");
         $("#contactPlacesTab").removeClass("hiddenTab");
         $("#contactFriendsTab").addClass("hiddenTab");
+        $("#contactDetailsTab").addClass("hiddenTab");
         
         $("#contactData").removeClass("activeTab");
         $("#contactHobbies").removeClass("activeTab");
         $("#contactPlaces").addClass("activeTab");
         $("#contactFriends").removeClass("activeTab");
+        $("#contactDetails").removeClass("activeTab");
     });
 
     $("#contactFriends").click(function(){
@@ -41,11 +47,41 @@ $(document).ready(function(){
         $("#contactHobbiesTab").addClass("hiddenTab");
         $("#contactPlacesTab").addClass("hiddenTab");
         $("#contactFriendsTab").removeClass("hiddenTab");
+        $("#contactDetailsTab").addClass("hiddenTab");
         
         $("#contactData").removeClass("activeTab");
         $("#contactHobbies").removeClass("activeTab");
         $("#contactPlaces").removeClass("activeTab");
         $("#contactFriends").addClass("activeTab");
+        $("#contactDetails").removeClass("activeTab");
+    });
+
+    $("#contactDetails").click(function(){
+        $("#contactDataTab").addClass("hiddenTab");
+        $("#contactHobbiesTab").addClass("hiddenTab");
+        $("#contactPlacesTab").addClass("hiddenTab");
+        $("#contactFriendsTab").addClass("hiddenTab");
+        $("#contactDetailsTab").removeClass("hiddenTab");
+        
+        $("#contactData").removeClass("activeTab");
+        $("#contactHobbies").removeClass("activeTab");
+        $("#contactPlaces").removeClass("activeTab");
+        $("#contactFriends").removeClass("activeTab");
+        $("#contactDetails").addClass("activeTab");
+    });
+
+    $("#contactSubmit").click(function(event){
+    	var firstName = $('#firstName').val();
+    	var lastName = $('#lastName').val();
+    	var birthDate = $('#birthDate').val();
+    	var version = $('#version').val();
+    	if ((firstName === "") || (firstName == undefined)) {
+    		alert("First name is required!");
+    	};
+    	if ((lastName === "") || (lastName == undefined)) {
+    		alert("Last name is required!");
+    	};
+    	event.preventDefault();
     });
 
 });
